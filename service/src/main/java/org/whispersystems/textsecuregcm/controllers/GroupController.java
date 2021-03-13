d27f5b33574af544fec6117aa2003c23b32bb406
xr.li <xr.li@tianrang-inc.com>
2021-03-13 14:45:16 +0800

    群组问题

---
+++ service/src/main/java/org/whispersystems/textsecuregcm/controllers/GroupController.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2013 Open WhisperSystems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.whispersystems.textsecuregcm.controllers;
+
+import com.codahale.metrics.annotation.Timed;
+import io.dropwizard.auth.Auth;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.whispersystems.textsecuregcm.storage.Account;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.*;
+
+
+@Path("/v1/groups")
+public class GroupController {
+
+  private final Logger logger = LoggerFactory.getLogger(GroupController.class);
+
+  public GroupController() {
+  }
+
+  @Timed
+  @PUT
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public String saveGroup(@Auth Optional<Account> source, String body) {
+    logger.warn(body);
+    return body;
+  }
+
+  @Timed
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public String getGroup(@Auth Account account, String body) {
+    logger.warn(body);
+    return body;
+  }
+
+  @Timed
+  @PATCH
+  public String patchGroup(@Auth Account account,
+                           String body) {
+    logger.warn(body);
+    return body;
+  }
+
+}
