77efdb538a52e918d11aafdce0b2d30434cecdc9
xr.li <xr.li@tianrang-inc.com>
2021-03-20 15:26:31 +0800

    epoch传0

--- service/src/main/java/org/whispersystems/textsecuregcm/controllers/GroupController.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/controllers/GroupController.java
@@ -283,7 +283,7 @@ public class GroupController {
                     var groupChangesNew=groupChanges.toBuilder();
                     groupChangesNew.addGroupChanges(GroupChanges.GroupChangeState.newBuilder()
                         .setGroupChange(groupChange)
-                        .setGroupState(group)
+                        .setGroupState(groupNew)
                         .build());
                     jedis.hset(GROUP_CHANGE_REDIS_KEY.getBytes(),groupKey.serialize(),groupChangesNew.build().toByteArray());
                 }
